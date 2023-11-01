e26bf0625fe077adb5b02afc35700d8ea0da9411
dennyX7 <prakash.vasireddi@gmail.com>
2023-11-01 11:41:38 +0530

    Add: complete code for todo app

--- src/declarations/todo_crud_backend/todo_crud_backend.did.d.ts
+++ src/declarations/todo_crud_backend/todo_crud_backend.did.d.ts
@@ -1,4 +1,17 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
-export interface _SERVICE { 'greet' : ActorMethod<[string], string> }
+export type TimestampMillis = bigint;
+export interface TodoItem {
+  'id' : number,
+  'added' : TimestampMillis,
+  'done' : boolean,
+  'name' : string,
+}
+export interface _SERVICE {
+  'add' : ActorMethod<[string], number>,
+  'delete_by_id' : ActorMethod<[number], boolean>,
+  'get_all' : ActorMethod<[[] | [boolean]], Array<TodoItem>>,
+  'get_by_id' : ActorMethod<[[] | [number]], Array<TodoItem>>,
+  'mark_done' : ActorMethod<[number], boolean>,
+}
--- src/declarations/todo_crud_backend/todo_crud_backend.did.js
+++ src/declarations/todo_crud_backend/todo_crud_backend.did.js
@@ -1,4 +1,21 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']) });
+  const TimestampMillis = IDL.Nat64;
+  const TodoItem = IDL.Record({
+    'id' : IDL.Nat32,
+    'added' : TimestampMillis,
+    'done' : IDL.Bool,
+    'name' : IDL.Text,
+  });
+  return IDL.Service({
+    'add' : IDL.Func([IDL.Text], [IDL.Nat32], []),
+    'delete_by_id' : IDL.Func([IDL.Nat32], [IDL.Bool], []),
+    'get_all' : IDL.Func([IDL.Opt(IDL.Bool)], [IDL.Vec(TodoItem)], ['query']),
+    'get_by_id' : IDL.Func(
+        [IDL.Opt(IDL.Nat32)],
+        [IDL.Vec(TodoItem)],
+        ['query'],
+      ),
+    'mark_done' : IDL.Func([IDL.Nat32], [IDL.Bool], []),
+  });
 };
 export const init = ({ IDL }) => { return []; };
